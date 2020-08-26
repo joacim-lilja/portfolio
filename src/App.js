ecc04c2d2499398d9027cc6c48bdebaf1eb710c1
Joacim Lilja <lijo1729@student.ju.se>
2020-08-26 10:46:58 +0200

    fixed header and footer

--- src/App.js
+++ src/App.js
@@ -10,72 +10,79 @@ import Projects from './components/Projects'
 
 function App() {
 
-  function hideFooter() {
-    var footer = document.getElementById('footer');
-    var header = document.getElementById('navbar')
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    if (scrolled > 90) {
-      footer.style.height = "5%";
-    } else {
-      footer.style.height = "0%";
-    }
-    if (scrolled < 10) {
-      header.style.height = "100px";
-    } else {
+  var winScroll = document.documentElement.scrollTop || window.pageYOffset;
+
+  function hideHeadFoot() {
+    var header = document.getElementById('navbar');
+    var buttons = document.getElementsByClassName('navbutton');
+    var title = document.getElementById('titleplaceholder');
+
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (winScroll < st) {
       header.style.height = "0px";
+      for (let index = 0; index < buttons.length; index++) {
+        buttons[index].style.fontSize = '0px';
+      }
+      title.style.fontSize = '0px';
+    } else {
+      header.style.height = "100px";
+      for (let index = 0; index < buttons.length; index++) {
+        buttons[index].style.fontSize = '24px';
+      }
+      title.style.fontSize = '40px';
     }
+    winScroll = st <= 0 ? 0 : st;
   };
 
-  window.onscroll = () => {hideFooter()};
+  window.onscroll = () => { hideHeadFoot() };
 
   return (
     <Router>
       <body style={{ 'background-image': '%PUBLIC_URL%/background.jpg' }}>
-        <div id="navbar">
-          <div id="leftnavbar">
-            <div className="buttondiv">
-              <Link to="/" id="titleplaceholder">Lilja/Home</Link>
+        <div id="page-container">
+          <div id="navbar">
+            <div id="leftnavbar">
+              <div className="buttondiv">
+                <Link to="/" id="titleplaceholder">Lilja/Home</Link>
+              </div>
+            </div>
+
+            <div id="rightnavbar">
+              <div className="buttondiv">
+                <Link to="/projects" class="navbutton">Projects</Link>
+              </div>
+              <div className="buttondiv">
+                <Link to="/about" class="navbutton">About</Link>
+              </div>
+              <div className="buttondiv">
+                <Link to="/contact" class="navbutton">Contact</Link>
+              </div>
             </div>
           </div>
 
-          <div id="rightnavbar">
-            <div className="buttondiv">
-              <Link to="/projects" class="navbutton">Projects</Link>
-            </div>
-            <div className="buttondiv">
-              <Link to="/about" class="navbutton">About</Link>
-            </div>
-            <div className="buttondiv">
-              <Link to="/contact" class="navbutton">Contact</Link>
-            </div>
+          <div id="content-wrap">
+            <Switch>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+
+              <Route path="/about">
+                <About />
+              </Route>
+
+              <Route path="/projects">
+                <Projects />
+              </Route>
+
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
           </div>
-        </div>
-
-
-
-        <div>
-          <Switch>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-
-            <Route path="/about">
-              <About />
-            </Route>
-
-            <Route path="/projects">
-              <Projects />
-            </Route>
-
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-        <div id="footer">
-          <h3>you can find my information on the contact page</h3>
+          <div id="footer">
+            <h3>you can find my information on the <Link to="/contact" styling={{"text-decoration" : "none"}}>contact page</Link></h3>
+          </div>
         </div>
       </body>
     </Router>
